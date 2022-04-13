539f13875491e49578425ca5151e635216a69568
Chase Childers <chasechilders12@gmail.com>
2022-04-13 17:00:41 -0400

    finished jeopardy functionality

--- main.py
+++ main.py
@@ -2,26 +2,71 @@ from game_title import game_board
 from question_functions import avengers_100
 from question_functions import avengers_500
 from question_functions import avengers_1000
+from question_functions import infinity_war100
+from question_functions import infinity_war500
+from question_functions import infinity_war1000
+from question_functions import endgame_100
+from question_functions import endgame_500
+from question_functions import endgame_1000 
+from jeopardy_board import jeopardy_boards
 
-points = [0]
+points = 0
 
 game_board()
 
+jeopardy_boards()
+
 def question_select():
+  global points
   movie_select = input("""
   What is the movie category you would like to answer questions from? """)
   amount_select = input("""
   What amount of money would you like to try and win? """)
   if movie_select == "The Avengers":
-    if amount_select == "100":
-      points += (avengers_100())
+    if amount_select == "$100":
+      points += avengers_100()
+      jeopardy_boards()
   if movie_select == "The Avengers":
     if amount_select == "$500":
-      points += (avengers_500())
+      points += avengers_500()
+      jeopardy_boards()
   if movie_select == "The Avengers":
     if amount_select == "$1000":
-      points += (avengers_1000())
-  
-
+      points += avengers_1000()
+      jeopardy_boards()
+  if movie_select == "Infinity War":
+    if amount_select == "$100":
+      points += infinity_war100()
+      jeopardy_boards()
+  if movie_select == "Infinity War":
+    if amount_select == "$500":
+      points += infinity_war500()
+      jeopardy_boards()
+  if movie_select == "Infinity War":
+    if amount_select == "$1000":
+      points += infinity_war1000()
+      jeopardy_boards()
+  if movie_select == "Endgame":
+    if amount_select == "$100":
+      points += endgame_100()
+      jeopardy_boards()
+  if movie_select == "Endgame":
+    if amount_select == "$500":
+      points += endgame_500()
+      jeopardy_boards()
+  if movie_select == "Endgame":
+    if amount_select == "$1000": 
+      points += endgame_1000()
+      jeopardy_boards()
+  if movie_select == "x":
+    if amount_select == "x":
+      print(f"""
+      
+      Thank you for playing!
+      
+      You finished with ${points}
+      
+      """)
+      exit()
 while True:
   question_select()
--- question_functions.py
+++ question_functions.py
@@ -4,14 +4,21 @@
 #   amount_select = input("What amount of money would you like to try and win? ")
 
 def avengers_100():
-  answer = input("""Who is the Director of S.H.I.E.L.D? 
+  answer = input("""  
+
+Who is the Director of S.H.I.E.L.D? 
+
     a) Phil Coulson
+
     b) Nick Fury
+
     c) Natasha Romanov
+
     d) Steve Rogers
     
     print a, b, c, or d
     
+
     """)
   if answer == "b":
       print("You got it correct! ")
@@ -22,13 +29,20 @@ def avengers_100():
 
 
 def avengers_500():
-  answer = input("""What is the item that Loki steals in order to take over earth?
+  answer = input("""  
+
+What is the item that Loki steals in order to take over earth?
+
   a) Thor's Hammer
+
   b) Captain America's Hammer
+
   c) Tesseract
+
   d) Helicopter
 
   print a, b, c, or d
+
 
   """)
   if answer == "c":
@@ -39,13 +53,20 @@ def avengers_500():
     return 0
 
 def avengers_1000():
-  answer = input("""What is used to destroy the wormhole generator that Loki built?
+  answer = input("""  
+
+What is used to destroy the wormhole generator that Loki built?
+
   a) The Hulk
+
   b) Loki's Scepter
+
   c) Nuclear Missile
+
   d) An Arrow
 
   print a, b, c, or d
+
 
   """)
   if answer == "b":
@@ -56,13 +77,20 @@ def avengers_1000():
     return 0
 
 def infinity_war100():
-  answer = input("""Who kills Loki in the beginning of Infinity War?
+  answer = input("""  
+
+Who kills Loki in the beginning of Infinity War?
+
   a) Thor
+
   b) The Hulk
+
   c) Tony Stark
+
   d) Thanos
 
   print a, b, c, or d
+
 
   """)
   if answer == "d":
@@ -73,13 +101,20 @@ def infinity_war100():
     return 0
 
 def infinity_war500():
-  answer = input("""Who attacked Thanos allowing him to break free of the Avengers and go to earth to retrieve the final stone?
+  answer = input("""  
+
+Who attacked Thanos allowing him to break free of the Avengers and go to earth to retrieve the final stone?
+
   a) Quill
+
   b) Peter Parker
+
   c) Nebula
+
   d) Drax
   
   print a, b, c, or d
+
 
   """)
   if answer == "a":
@@ -90,13 +125,20 @@ def infinity_war500():
     return 0
 
 def infinity_war1000():
-  answer = input("""What planet were Tony and Nebula stranded on after Thanos snapped his fingers?
+  answer = input("""  
+
+What planet were Tony and Nebula stranded on after Thanos snapped his fingers?
+
   a) Earth
+
   b) Nidavellir
+
   c) Titan
+
   d) Knowhere
 
   print a, b, c, or d
+
 
   """)
   if answer == "c":
@@ -107,14 +149,21 @@ def infinity_war1000():
     return 0
 
 def endgame_100():
-  answer = input("""Who rescued Tony and Nebula from the space ship in the beginning of Endgame?
+  answer = input("""  
+
+Who rescued Tony and Nebula from the space ship in the beginning of Endgame?
+
   a) Captain America
+
   b) Thanos
+
   c) Dr. Strange
+
   d) Captain Marvel
 
   print a, b, c, or d
   
+
   """)
   if answer == "d":
     print("You got it right! ")
@@ -124,13 +173,20 @@ def endgame_100():
     return 0
 
 def endgame_500():
-  answer = input("""When the Avengers went back in time, where did Rocket take the Time Stone from?
+  answer = input("""  
+  
+When the Avengers went back in time, where did Rocket take the Time Stone from?
+
   a) Tony's Briefcase
+
   b) Loki's Room
+
   c) Thor's Mom
+
   d) Jane's pocket
   
   print a, b, c, or d
+
 
   """)
   if answer == "d":
@@ -141,12 +197,21 @@ def endgame_500():
     return 0
 
 def endgame_1000():
-  answer = input("""After Tony's death and funeral, who was appointed the new ruler of Asgard?
+  answer = input(""" 
+
+After Tony's death and funeral, who was appointed the new ruler of Asgard?
+
   a) Valkyrie
+
   b) Rocket
+
   c) Thor
+
   d) Stephen Strange
   
+  print a, b, c, or d
+
+
   """)
   if answer == "a":
     print("You got it right! ")
