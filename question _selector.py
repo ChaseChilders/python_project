e44a3e96c808effbfb0cad9c7c40d689666db98c
Chase Childers <chasechilders12@gmail.com>
2022-04-13 12:06:37 -0400

    added new question to project

---
+++ question _selector.py
@@ -0,0 +1,30 @@
+player_total = 0
+
+def question_select():
+  question_total = 100
+  movie_select = input("What is the movie category you would like to answer questions from? ")
+  amount_select = input("What amount of money would you like to try and win? ")
+  if movie_select == "The Avengers":
+    if amount_select == "$100":
+      avengers_100 = input("""Who is the Director of S.H.I.E.L.D? 
+      a) Phil Coulson
+      b) Nick Fury
+      c) Natasha Romanov
+      d) Steve Rogers
+
+      print a, b, c, or d
+      """)
+      if avengers_100 == "b":
+        print("You got it correct!")
+        player_new_total = player_total + question_total
+        print(f"Your total is ${player_new_total}")
+      else:
+        print("You got it wrong :( ")
+        print(f"Your total is {player_total}")
+  if movie_select == "The Avengers":
+    if amount_select == "$500":
+      avengers_500 = input("""What is the item that Loki steals in order to take over earth?
+      a) Thor's Hammer
+      b) Captain America's Hammer
+      c) Tesseract
+      d) Hel""")
